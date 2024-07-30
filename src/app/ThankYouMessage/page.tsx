7b25224187696a9489e85a95421c38c71c4d209c
ramya72002 <nsriramya7@gmail.com>
2024-07-30 09:58:00 +0530

    admin page added

--- src/app/ThankYouMessage/page.tsx
+++ src/app/ThankYouMessage/page.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+'use client'
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import './ThankYouMessage.scss';
 
 const ThankYouMessage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push('/');
+    }, 2000); // Redirect after 3 seconds
+
+    return () => clearTimeout(timer); // Clear timeout if component unmounts
+  }, [router]);
+
   return (
     <div className="thank-you-container">
       <img src="/images/thank1.png" alt="Thank You Icon" className="thank-you-icon" />
-     
       <h1>Your submission has been received.</h1>
     </div>
   );
